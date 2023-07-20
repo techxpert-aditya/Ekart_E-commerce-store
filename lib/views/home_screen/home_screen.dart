8085dbf4578dd981f0bf06757c45e4059e96157a
aditya piyushbhai siddhapura <techxpert.aditya2409@gmail.com>
2023-07-20 23:07:08 +0530

    minor fixes: scroll behaviour added with a bouncing scrollview physics

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -49,98 +49,108 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
 
           (context.screenHeight * 0.03).heightBox,
-          // swiper brands
-          VxSwiper.builder(
-            aspectRatio: 16 / 9,
-            autoPlay: true,
-            height: (context.screenHeight * 0.17),
-            enlargeCenterPage: true,
-            itemCount: homeFirstSwiperList.length,
-            itemBuilder: (context, index) {
-              return Image.asset(
-                homeFirstSwiperList[index],
-                fit: BoxFit.fill,
-              )
-                  .box
-                  .rounded
-                  .clip(Clip.antiAlias)
-                  .margin(const EdgeInsets.symmetric(horizontal: 8))
-                  .make();
-            },
-          ),
 
-          (context.screenHeight * 0.02).heightBox,
-          // selections
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: List.generate(
-              2,
-              (index) => homeTile(
-                height: context.screenHeight * 0.15,
-                width: context.screenWidth / 2.25,
-                icon: index == 0 ? icTodaysDeal : icFlashDeal,
-                label: index == 0 ? todaysDeal : flashSale,
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  // swiper brands
+                  VxSwiper.builder(
+                    aspectRatio: 16 / 9,
+                    autoPlay: true,
+                    height: (context.screenHeight * 0.17),
+                    enlargeCenterPage: true,
+                    itemCount: homeFirstSwiperList.length,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        homeFirstSwiperList[index],
+                        fit: BoxFit.fill,
+                      )
+                          .box
+                          .rounded
+                          .clip(Clip.antiAlias)
+                          .margin(const EdgeInsets.symmetric(horizontal: 8))
+                          .make();
+                    },
+                  ),
+
+                  (context.screenHeight * 0.02).heightBox,
+                  // selections
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                      2,
+                      (index) => homeTile(
+                        height: context.screenHeight * 0.15,
+                        width: context.screenWidth / 2.25,
+                        icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                        label: index == 0 ? todaysDeal : flashSale,
+                      ),
+                    ),
+                  ),
+
+                  (context.screenHeight * 0.02).heightBox,
+
+                  // second swiper
+                  VxSwiper.builder(
+                    aspectRatio: 16 / 9,
+                    autoPlay: true,
+                    height: (context.screenHeight * 0.17),
+                    enlargeCenterPage: true,
+                    itemCount: homeSecondSwiperList.length,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        homeSecondSwiperList[index],
+                        fit: BoxFit.fill,
+                      )
+                          .box
+                          .rounded
+                          .clip(Clip.antiAlias)
+                          .margin(const EdgeInsets.symmetric(horizontal: 8))
+                          .make();
+                    },
+                  ),
+
+                  (context.screenHeight * 0.02).heightBox,
+
+                  // top catagories
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                      3,
+                      (index) => homeTile(
+                        height: context.screenHeight * 0.15,
+                        width: context.screenWidth / 3.5,
+                        icon: index == 0
+                            ? icTopCategories
+                            : index == 1
+                                ? icBrands
+                                : icTopSeller,
+                        label: index == 0
+                            ? topCatagories
+                            : index == 1
+                                ? brands
+                                : topSellers,
+                      ),
+                    ),
+                  ),
+
+                  (context.screenHeight * 0.02).heightBox,
+
+                  // featured catagories
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: featuredCatagories.text
+                        .size(18)
+                        .color(darkFontGrey)
+                        .fontFamily(semiBold)
+                        .make(),
+                  ).box.margin(const EdgeInsets.only(left: 16)).make(),
+                ],
               ),
             ),
-          ),
-
-          (context.screenHeight * 0.02).heightBox,
-
-          // second swiper
-          VxSwiper.builder(
-            aspectRatio: 16 / 9,
-            autoPlay: true,
-            height: (context.screenHeight * 0.17),
-            enlargeCenterPage: true,
-            itemCount: homeSecondSwiperList.length,
-            itemBuilder: (context, index) {
-              return Image.asset(
-                homeSecondSwiperList[index],
-                fit: BoxFit.fill,
-              )
-                  .box
-                  .rounded
-                  .clip(Clip.antiAlias)
-                  .margin(const EdgeInsets.symmetric(horizontal: 8))
-                  .make();
-            },
-          ),
-
-          (context.screenHeight * 0.02).heightBox,
-
-          // top catagories
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: List.generate(
-              3,
-              (index) => homeTile(
-                height: context.screenHeight * 0.15,
-                width: context.screenWidth / 3.5,
-                icon: index == 0
-                    ? icTopCategories
-                    : index == 1
-                        ? icBrands
-                        : icTopSeller,
-                label: index == 0
-                    ? topCatagories
-                    : index == 1
-                        ? brands
-                        : topSellers,
-              ),
-            ),
-          ),
-
-          (context.screenHeight * 0.02).heightBox,
-
-          // featured catagories
-          Align(
-            alignment: Alignment.centerLeft,
-            child: featuredCatagories.text
-                .size(18)
-                .color(darkFontGrey)
-                .fontFamily(semiBold)
-                .make(),
-          ).box.margin(const EdgeInsets.only(left: 16)).make(),
+          )
         ],
       )),
     );
