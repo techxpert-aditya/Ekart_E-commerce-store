3f313d93e554fdbe7896a1726ca9198fbac89440
aditya piyushbhai siddhapura <techxpert.aditya2409@gmail.com>
2023-07-20 18:53:26 +0530

    The Bottom Nav bar is now working in home screen

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,10 +1,16 @@
 import 'package:emart/consts/consts.dart';
+import 'package:emart/controllers/home_controller.dart';
+import 'package:get/get.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 // import 'package:flutter/src/widgets/placeholder.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
+  // creating the home controller for the tabs
+  final HomeController controller = Get.put(HomeController());
+
+  // navbar item
   final List<BottomNavigationBarItem> navBarItems = [
     BottomNavigationBarItem(
         icon: Image.asset(
@@ -32,12 +38,43 @@ class HomeScreen extends StatelessWidget {
         label: account),
   ];
 
+  final List<Widget> navBody = [
+    Container(color: Colors.amber),
+    Container(color: Colors.blue),
+    Container(color: Colors.greenAccent),
+    Container(color: Colors.cyan),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
-      body: Container(),
-      // bottomNavigationBar: BottomNavigationBar(items: list),
+      // backgroundColor: Colors.amber,
+      body: Column(
+        children: [
+          Expanded(
+            child: Obx(
+              () => navBody.elementAt(controller.currentNavIndex.value),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          currentIndex: controller.currentNavIndex.value,
+          backgroundColor: whiteColor,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          selectedItemColor: redColor,
+          selectedLabelStyle: const TextStyle(
+            fontFamily: semibold,
+          ),
+          items: navBarItems,
+          onTap: (value) {
+            controller.currentNavIndex.value = value;
+          },
+        ),
+      ),
     );
   }
 }
